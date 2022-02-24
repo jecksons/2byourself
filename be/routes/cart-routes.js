32183c6c85e5563591efe492139922a995c0069a
Jeckson <jeckson.es@gmail.com>
2022-02-24 13:14:50 -0300

    Adjust de client version control

--- be/routes/cart-routes.js
+++ be/routes/cart-routes.js
@@ -2,9 +2,9 @@ const authController = require('../controllers/auth-controller')
 const controller = require('../controllers/cart-controller');
 
 module.exports = (app, handleRequestDB, express)   => {   
-   app.post('/carts/',  (req, res) => handleRequestDB(req, res, controller.addItemToCartReq));      
-   app.get('/carts/id/:id',  (req, res) => handleRequestDB(req, res, controller.getCartReq));         
-   app.post('/carts/items/',  (req, res) => handleRequestDB(req, res, controller.updateCartItemReq));      
-   app.delete('/carts/items/:id',  (req, res) => handleRequestDB(req, res, controller.delItemFromCartReq));      
+   app.post('/carts/',  [authController.verifyClientVersion], (req, res) => handleRequestDB(req, res, controller.addItemToCartReq));      
+   app.get('/carts/id/:id', [authController.verifyClientVersion], (req, res) => handleRequestDB(req, res, controller.getCartReq));         
+   app.post('/carts/items/', [authController.verifyClientVersion], (req, res) => handleRequestDB(req, res, controller.updateCartItemReq));      
+   app.delete('/carts/items/:id', [authController.verifyClientVersion], (req, res) => handleRequestDB(req, res, controller.delItemFromCartReq));      
    
 }
--- be/routes/product-routes.js
+++ be/routes/product-routes.js
@@ -3,13 +3,13 @@ const controller = require('../controllers/product-controller');
 
 module.exports = (app, handleRequestDB, express)   => {   
    
-   app.use('/products/img', express.static('./media/img/done'));  
-   app.get('/products/filters',  (req, res) => handleRequestDB(req, res, controller.getProductFiltersReq));   
-   app.get('/products/menu',  (req, res) => handleRequestDB(req, res, controller.getMenuOptionsReq));      
-   app.get('/products/',  (req, res) => handleRequestDB(req, res, controller.getProductsReq));   
-   app.get('/products/id/:id',  (req, res) => handleRequestDB(req, res, controller.getProductByIdReq));   
-   app.get('/products/generate-ratings/',  (req, res) => handleRequestDB(req, res, controller.generateRatingsReq) );
-   app.get('/products/search-text-options/',  (req, res) => handleRequestDB(req, res, controller.getProductsOptionsByTextReq) );
+   app.use('/products/img', [authController.verifyClientVersion], express.static('./media/img/done'));  
+   app.get('/products/filters',  [authController.verifyClientVersion], (req, res) => handleRequestDB(req, res, controller.getProductFiltersReq));   
+   app.get('/products/menu', [authController.verifyClientVersion], (req, res) => handleRequestDB(req, res, controller.getMenuOptionsReq));      
+   app.get('/products/', [authController.verifyClientVersion], (req, res) => handleRequestDB(req, res, controller.getProductsReq));   
+   app.get('/products/id/:id', [authController.verifyClientVersion], (req, res) => handleRequestDB(req, res, controller.getProductByIdReq));   
+   app.get('/products/generate-ratings/', [authController.verifyClientVersion], (req, res) => handleRequestDB(req, res, controller.generateRatingsReq) );
+   app.get('/products/search-text-options/', [authController.verifyClientVersion], (req, res) => handleRequestDB(req, res, controller.getProductsOptionsByTextReq) );
 
    
 }
